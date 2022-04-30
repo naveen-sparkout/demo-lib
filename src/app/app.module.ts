0c2d9c161566ea134c17d3b73b08bd68300fe70b
“naveen-sparkout” <naveenkumar@sparkouttech.com>
2022-04-30 09:40:44 +0530

    first one

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyLibComponent } from 'my-lib';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyLibComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
